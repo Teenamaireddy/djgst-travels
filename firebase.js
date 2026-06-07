129c1542aa21764bd73e51413f4e36ae63e6bb02
Teenamaireddy <mchinniworldstar@gmail.com>
2026-06-07 08:54:03 +0530

    Update Firebase SDK imports and add success log
    Updated Firebase SDK imports to use specific version URLs and added a success log message.

--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDNtl9Vt16P8DE0g8rk263nifdR4zl7Wnk",
   authDomain: "djgst-travels.firebaseapp.com",
@@ -16,6 +11,7 @@ const firebaseConfig = {
   measurementId: "G-0JLW90GQ1S"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+console.log("Firebase Connected Successfully!");
