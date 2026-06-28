c205bc59717651c107821d89bf3a77ef8de804bd
Teenamaireddy <mchinniworldstar@gmail.com>
2026-06-28 18:14:36 +0530

    Implement service worker registration
    Add service worker registration for Firebase messaging.

--- common.js
+++ common.js
@@ -1,1 +1,10 @@
-
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", async () => {
+        try {
+            const registration = await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+            console.log("✅ Service Worker Registered:", registration);
+        } catch (error) {
+            console.error("❌ Service Worker Registration Failed:", error);
+        }
+    });
+}
