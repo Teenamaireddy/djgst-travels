f4d4b31bc8cdc2615e3ac5540548a073d8310e6f
Teenamaireddy <mchinniworldstar@gmail.com>
2026-06-28 17:58:17 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -16,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const messaging = getMessaging(app);
 
-export { auth, db };
+export { auth, db, messaging };
