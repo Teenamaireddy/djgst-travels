546a794cee6f88d024b2ffd0f908cadc5f96f3ae
Teenamaireddy <mchinniworldstar@gmail.com>
2026-07-09 11:26:13 +0530

    Update memory-store.js

--- ai/memory/memory-store.js
+++ ai/memory/memory-store.js
@@ -1,1 +1,42 @@
 
+/**
+ * =====================================
+ * DJGST AI Memory Engine
+ * =====================================
+ */
+
+class MemoryStore {
+
+    constructor() {
+
+        this.memory = {};
+
+    }
+
+    save(key, value) {
+
+        this.memory[key] = value;
+
+    }
+
+    get(key) {
+
+        return this.memory[key];
+
+    }
+
+    getAll() {
+
+        return this.memory;
+
+    }
+
+    clear() {
+
+        this.memory = {};
+
+    }
+
+}
+
+export default new MemoryStore();
