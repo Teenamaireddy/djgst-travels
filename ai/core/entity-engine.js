5dad0f7307f756f6e4d0dbbc64b9865fa8d6108e
Teenamaireddy <mchinniworldstar@gmail.com>
2026-07-13 08:23:39 +0530

    Update entity-engine.js

--- ai/core/entity-engine.js
+++ ai/core/entity-engine.js
@@ -8,17 +8,46 @@ class EntityEngine {
 
     extract(userMessage) {
 
-        return {
-            transport: null,
-            from: null,
-            to: null,
-            date: null,
-            adults: null,
-            children: null
-        };
+    const text = userMessage.toLowerCase();
+
+    const entities = {
+
+        transport: null,
+        from: null,
+        to: null,
+        date: null,
+        adults: null,
+        children: null
+
+    };
+
+    // Detect Bus
+    if (text.includes("bus")) {
+
+        entities.transport = "Bus";
 
     }
 
+    // Detect Train
+    else if (text.includes("train")) {
+
+        entities.transport = "Train";
+
+    }
+
+    // Detect Flight
+    else if (
+        text.includes("flight") ||
+        text.includes("plane")
+    ) {
+
+        entities.transport = "Flight";
+
+    }
+
+    return entities;
+
+    }
 }
 
 export default new EntityEngine();
