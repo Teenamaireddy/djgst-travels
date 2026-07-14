728d671901bff9cc31fdeaa51ddb6b54bd84a2be
Teenamaireddy <mchinniworldstar@gmail.com>
2026-07-14 17:18:39 +0530

    Update djgst-ai.js

--- djgst-ai.js
+++ djgst-ai.js
@@ -1,4 +1,7 @@
 import djgstAI from "./ai/core/ai.js";
+import intentEngine from "./ai/core/intent-engine.js";
+import entityEngine from "./ai/core/entity-engine.js";
+import slotEngine from "./ai/core/slot-engine.js";
 
 const AI_DEVELOPER_MODE = true;
 const allowedPages = [
