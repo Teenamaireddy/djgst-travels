8d6642bd200c164deb67cfc9ea3aa068c6550866
Teenamaireddy <mchinniworldstar@gmail.com>
2026-07-15 08:59:38 +0530

    Create cities.js

---
+++ ai/data/cities.js
@@ -0,0 +1,16 @@
+const cities = [
+
+    "Vizag",
+    "Visakhapatnam",
+    "Rajahmundry",
+    "Vijayawada",
+    "Hyderabad",
+    "Chennai",
+    "Bangalore",
+    "Delhi",
+    "Mumbai",
+    "Tirupati"
+
+];
+
+export default cities;
