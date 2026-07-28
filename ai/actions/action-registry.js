14f07505ad107d9142aabdbd6e97d695a472721e
Teenamaireddy <mchinniworldstar@gmail.com>
2026-07-28 11:42:52 +0530

    Update action-registry.js

--- ai/actions/action-registry.js
+++ ai/actions/action-registry.js
@@ -1,1 +1,34 @@
 
+class ActionRegistry {
+
+    execute(intent, memory) {
+
+        switch (intent) {
+
+            case "book_ticket":
+
+                return {
+
+                    action: "search_bus",
+
+                    data: memory
+
+                };
+
+            default:
+
+                return {
+
+                    action: null
+
+                };
+
+        }
+
+    }
+
+}
+
+export default new ActionRegistry();
+
+
